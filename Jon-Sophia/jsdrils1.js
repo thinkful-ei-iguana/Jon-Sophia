5e931d7538a020208c74bf17948cab97c081aa71
Jon <jonathanjackson424@gmail.com>
2019-10-21 15:52:47 -0400

    added try and catch

--- Jon-Sophia/jsdrils1.js
+++ Jon-Sophia/jsdrils1.js
@@ -1,20 +1,25 @@
 /* eslint-disable strict */
-let yearOfBirth = 2019;
-
-
-
 function createGreeting(name, age){
   return `Hi, my name is ${name} and I am ${age} years old.`;
 }
 
 function getYearOfBirth(age){
+  if (age < 0) {
+    throw new Error("Age can not be negative");
+  }
   let born = 2019 - age;
   return `I was born in ${born}`;
 }
 
+try {
+  const greeting1 = createGreeting(Jon, -11);
+} catch(err){
+  console.error(Error);
+} 
+
 
 console.log(createGreeting('Jon', 27));
 
-console.log(getYearOfBirth(27));
+console.log(getYearOfBirth(-2));
 
 
