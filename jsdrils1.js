ab0555faf289a4a73141d0e87fb3894f1247cdd1
Jon <jonathanjackson424@gmail.com>
2019-10-21 16:42:29 -0400

    this should be it

--- jsdrils1.js
+++ jsdrils1.js
@@ -1,8 +1,27 @@
 /* eslint-disable strict */
 function createGreeting(name, age){
-  return `Hi, my name is ${name} and I am ${age} years old.`;
+  if (name === undefined || age === undefined){
+    throw new Error("Arguments not valid");
+  } if (age < 0) {
+    throw new Error("Age can not be negative");
+  }
+  let born = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${born}.`;
 }
 
-console.log(createGreeting('Jon', 27));
+function getYearOfBirth(age){
+  let born = 2019 - age;
+  return born;
+}
+
+
+try {
+  const greeting1 = createGreeting(19);
+  console.log(createGreeting('Jon', 27));
+} catch(err){
+  console.error(err);
+} 
+
+console.log(getYearOfBirth(-2));
 
 
